591b7ec642888e0fa0c45553f118172b9cd2f7e5
smizuoch <smizuoch@student.42tokyo.jp>
2025-11-13 16:46:06 +0900

    main.cppの色出力処理をcolorクラスを使用するようにリファクタリング

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "vec3.h"
+#include "color.h"
 #include <iostream>
 
 int	main(int ac, char **av)
@@ -14,18 +16,14 @@ int	main(int ac, char **av)
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 	for(int j = image_height - 1; j >= 0; --j)
 	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i)
 		{
-			std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-			double r = double(i) / (image_width - 1);
-			double g = double(j) / (image_height - 1);
-			double b = 0.25;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			color pixel_color(
+				double(i) / (image_width - 1),
+				double(j) / (image_height - 1),
+				0.25);
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nDone.\n";
