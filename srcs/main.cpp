07cb6540b4e52aa51a8137be53d8c466992e724b
smizuoch <smizuoch@student.42tokyo.jp>
2025-11-14 19:09:27 +0900

    hit_sphere関数を追加し、レイが球体に当たった場合に赤色を返すようにray_color関数を修正

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,8 +3,20 @@
 #include "rah.h"
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(oc, r.direction());
+	double c = dot(oc, oc) - radius*radius;
+	double discriminant = b*b - 4*a*c;
+	return (discriminant > 0);
+}
+
 color ray_color(const ray& r)
 {
+	if(hit_sphere(point3(0,0,-1), 0.5, r))
+		return color(1,0,0);
 	vec3 unit_direction = unit_vector(r.direction());
 	double t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
