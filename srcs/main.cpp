74efa779609ab386bc368b6ea9f54711f70c5081
smizuoch <smizuoch@student.42tokyo.jp>
2025-11-14 16:49:13 +0900

    ray_color関数を追加し、画像生成のためのレイを計算するロジックを実装

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,14 @@
 #include "vec3.h"
 #include "color.h"
+#include "rah.h"
 #include <iostream>
+
+color ray_color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	double t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int	main(int ac, char **av)
 {
@@ -11,19 +19,31 @@ int	main(int ac, char **av)
 		return 1;
 	}
 
-	const int image_width = 256;
-	const int image_height = 256;
+	const double aspect_ratio = 16.0 / 9.0;
+	const int image_width = 384;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
+
+	point3 origin = point3(0, 0, 0);
+	vec3 horizontal = vec3(viewport_width, 0, 0);
+	vec3 vertical = vec3(0, viewport_height, 0);
+	point3 lower_left_corner =
+		origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
 	for(int j = image_height - 1; j >= 0; --j)
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i)
 		{
-			color pixel_color(
-				double(i) / (image_width - 1),
-				double(j) / (image_height - 1),
-				0.25);
+			double u = double(i) / (image_width - 1);
+			double v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
