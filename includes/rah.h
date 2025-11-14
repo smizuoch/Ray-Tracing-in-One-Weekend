27f4ef60e2cb192bd957776c5593f32f30da3291
smizuoch <smizuoch@student.42tokyo.jp>
2025-11-14 15:01:50 +0900

    rah.hを新規作成し、レイを表すクラスを定義

---
+++ includes/rah.h
@@ -0,0 +1,27 @@
+#ifndef RAH_H
+#define RAH_H
+
+#include "vec3.h"
+
+// レイを表すクラス
+class ray
+{
+public:
+	ray() {}
+	ray(const point3& origin, const vec3& direction)
+		: orig(origin), dir(direction) {}
+
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+
+	point3 at(double t) const
+	{
+		return orig + t*dir;
+	}
+
+public:
+	point3 orig;
+	vec3 dir;
+};
+
+#endif
